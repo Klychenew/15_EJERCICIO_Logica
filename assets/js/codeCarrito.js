e33dfcf96f853d89677dca827536e027b7b62c85
Klychenew <calichenew@gmail.com>
2022-08-12 15:26:44 -0500

    adelanto en pag carrito

---
+++ assets/js/codeCarrito.js
@@ -0,0 +1,33 @@
+let database_catalogoComida = {
+    "productos": [{
+        id: "1",
+        nombre: "Combo de Lujo",
+        precio: 25000,
+        img: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/9nRURGj1MQf25aeJK0n48TqY9Sz1%2F6B1EA773-7BF2-4C8B-AF80-202D31690B24.jpg?alt=media",
+    },
+    {
+        id: "2",
+        nombre: "Perro Caliente",
+        precio: 18000,
+        img: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/9nRURGj1MQf25aeJK0n48TqY9Sz1%2Fthumb_280_CB6B878C-AE98-4722-A675-C3076BED4212.jpg?alt=media",
+    },
+    {
+        id: "3",
+        nombre: "Papa Francesa",
+        precio: 10000,
+        img: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/9nRURGj1MQf25aeJK0n48TqY9Sz1%2Fthumb_280_8B89C428-584F-4233-A6EB-D6640689DB64.jpg?alt=media",
+    },
+    {
+        id: "4",
+        nombre: "Pollo Desmechado",
+        precio: 10000,
+        img: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/9nRURGj1MQf25aeJK0n48TqY9Sz1%2Fthumb_280_CE2C4FD0-D892-41A4-86F2-6E628E3F5A56.jpg?alt=media",
+    },
+    {
+        id: "5",
+        nombre: "Cerveza BBC",
+        precio: 10000,
+        img: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/9nRURGj1MQf25aeJK0n48TqY9Sz1%2Fthumb_280_29944217-A3CD-46E5-9DD4-7EBCF8E8276D.jpg?alt=media",
+    },
+]
+}
